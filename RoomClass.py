505651596606a99bc47043a2cf6d61214a60a9c5
intermezzio <anmascillaro@ctemc.org>
2018-01-26 09:52:51 -0500

    Update Room Constructor

--- RoomClass.py
+++ RoomClass.py
@@ -5,7 +5,7 @@ Creates the rooms
 
 class Room:
     
-    def __init__(self, name, number, objects, key=False, lock=None):
+    def __init__(self, name, number, objs, key=False, light=True, lock=None):
         """
         :param name: room name
         :param number: room number
@@ -15,13 +15,24 @@ class Room:
         """
         self.name = name
         self.number = number
-        self.objects = objects
+        self.objs = objs
         self.key = key
+        self.light = True
         
-    
+    def pickObj(self, obj, user):
+        if obj in self.objs:
+            if user.space > 0:
+                self.objs.remove(obj)
+                user.objects.append(obj)
+            else:
+                return "No space available"
+        else:
+            return "Object not found"
+        return "Done"
+                
     
     
 """
 Example code
 """
-chemLab = Room("Chemistry Lab", 140, ["One chemical", "Other chemical"], False, None)
+chemLab = Room("Chemistry Lab", 140, ["One chemical", "Other chemical"])

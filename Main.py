c4b7f18ffa509509ac2465676e03174778b32e53
mahorchler <mahorchler@ctemc.org>
2018-02-16 13:55:20 -0500

    More

--- Main.py
+++ Main.py
@@ -2,13 +2,17 @@ from NpcClass import *
 from UserClass import *
 from RoomClass import *
 from ItemClass import *
+from StorageClass import *
 
 """
 Starts and Ends Game
 """
 
 def createObjects():
-    furniture = ["front cabinet", "back cabinet", "front desk", "phone charging station"]
+    furniture = [StorageClass.Storage("front cabinet",2, None, "cabinet in the front of the room"),
+                StorageClass.Storage("back cabinet",2, None, "cabinet in the back of the room"),
+                StorageClass.Storage("front desk",2, None, "desk in the front of the room"),
+                StorageClass.Storage("phone charging station",1,None, "phone charging station")]
     Rm170 = RoomClass.Room("CSE Classroom", 170, furniture, True) 
     Hanas = NpcClass.boss("Mr. Hanas", "roll", 1, None)
     

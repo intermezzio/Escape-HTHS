0ae0072ae802b7ec595982aa74117d112c79c83a
Andrew M <anmascillaro@ctemc.org>
2018-02-20 15:53:50 -0500

    More methods in the item class
    Please note the Item.clone method, it is pretty important.

--- ItemClass.py
+++ ItemClass.py
@@ -2,7 +2,7 @@
 Creates Items and Furniture.
 """
 
-class item:
+class Item:
     def __init__(self, name, function, description):
         """
         :param name: name of item
@@ -11,3 +11,20 @@ class item:
         self.name = name
         self.function = function
         self.description = description
+
+    def getName(self):
+    	return self.name
+
+    def getFunction(self):
+    	return self.function
+
+    def getDescription(self):
+    	return self.getDescription
+
+    def clone(self, name=self.name, function=self.function, description=self.description):
+    	"""
+		Creates a copy of an object (ex. create two different items of the same type without referring to the same section in memory)
+		All attributes can be changed as optional parameters
+    	"""
+    	clone = Item(name, function, description)
+    	return clone

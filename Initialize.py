924ccb8173fc068e13878fb4a288068f37222f2f
Anna Cai <anana10c@gmail.com>
2018-02-20 20:36:30 -0500

    initialized objects

--- Initialize.py
+++ Initialize.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Initialize.py
 
@@ -26,7 +27,10 @@ Pencil = Item("Pencil", f1, "A pencil. Can be used to write, among other uses.")
 Frisbee = Item("Frisbee", f1, "An orange disc that can fly if thrown properly.")
 SafetyRules = Item("Safety Rules", f1, "A list of safety rules for the chem lab and tech lab. I hope you passed your quiz...")
 Apron = Item("Apron", f1, "A chemical-resistant apron. Must be worn in the chem lab.")
-
+TechKey = Item("Tech Lab Key", f1, "A key that can lock or unlock the door to the tech lab.")
+Goggles = Item("Goggles", f1, "A pair of goggles. Used for eye protection. Must be worn in the chem lab and in the tech lab.")
+Lightbulb = Item("Lightbulb", f1, "How many high techers does it take to screw in a lightbulb?")
+Calculator = Item("TI-Nspire", f1, "It may just be a calculator, but it’s a very powerful tool.")
 
 ########
 # Create Storage

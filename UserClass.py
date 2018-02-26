de65733ba5c79b1dd6d41351a35106cb72e633b1
Anna Cai <anana10c@gmail.com>
2018-02-26 03:56:24 -0500

    deleted test line

--- UserClass.py
+++ UserClass.py
@@ -99,7 +99,6 @@ class User:
 	    itemDict = dict()
 	    for item in self.items:
 	        if item.getDamage() > 0:
-	           print item.getDamage()
 	           itemDict[item.getName()] = item.getDescription()
 	     
 	    return itemDict
